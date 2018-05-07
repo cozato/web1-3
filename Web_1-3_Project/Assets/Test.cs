b516027b18881a84669b196e1e1edf7f7a17c199
tkskd <g1727038@st.t-kougei.ac.jp>
2018-05-07 17:49:38 +0900

    変数_課題_ベクタークラスまで

--- Web_1-3_Project/Assets/Test.cs
+++ Web_1-3_Project/Assets/Test.cs
@@ -2,9 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Player//クラスの作成
+{
+
+    private int hp = 100;
+    private int power = 50;
+
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+    public void Damage(int damage)
+    {
+        this.hp -= damage;
+        Debug.Log(damage + "のダメージを受けた");
+    }
+}
+
+
 public class Test : MonoBehaviour {
 
-	// Use this for initialization
+    // Use this for initialization
+
+    
+    void SayHello()//引数も返り値もないメソッド
+    {
+        Debug.Log("Hello");
+    }
+
+    void CallName(string name)//引数のあるメソッド
+    {
+        Debug.Log("Hello" + name);
+    }
+
+    int Add(int a, int b)//引数と返り値のあるメソッド
+    {
+        int c = a + b;
+
+        return c;
+    }
+
 	void Start () {
         //Hello, Worldと表示
         Debug.Log("Hello, World");
@@ -121,6 +158,60 @@ public class Test : MonoBehaviour {
             sum += 1;
         }
         Debug.Log(sum);
+
+        //配列の使い方
+        int[] array = new int[5];
+
+        array[0] = 2;
+        array[1] = 10;
+        array[2] = 5;
+        array[3] = 15;
+        array[4] = 3;
+
+        for(int i=0; i<5; i++)
+        {
+            Debug.Log(array[i]);
+        }
+
+        //配列の初期化
+        int[] points = { 83, 99, 52, 93, 15 };
+
+        for(int i=0; i< points.Length; i++)
+        {
+            if (points[i] >= 90)
+            {
+                Debug.Log(points[i]);
+            }
+        }
+
+        //引数も返り値もないメソッド
+        SayHello();
+
+        //引数のあるメソッド
+        CallName("Tom");
+
+        //引数と返り値のあるメソッド
+        answer = Add(2, 3);
+        Debug.Log(answer);
+
+        //クラスの作成
+        Player myPlayer = new Player();
+        myPlayer.Attack();
+        myPlayer.Damage(30);
+
+        //Vectorクラスの使い方
+        Vector2 playerPos = new Vector2(3.0f, 4.0f);
+        playerPos.x += 8.0f;
+        playerPos.y += 5.0f;
+        Debug.Log(playerPos);
+
+        Vector2 startPos = new Vector2(2.0f, 1.0f);
+        Vector2 endPos = new Vector2(8.0f, 5.0f);
+        Vector2 dir = endPos - startPos;
+        Debug.Log(dir);
+
+        float len = dir.magnitude;
+        Debug.Log(len);
     }
 
     // Update is called once per frame
